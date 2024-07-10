db4f8f39cafc0339a13a674160be06b39605a41e
zymouse <zymouse@pixmoving.net>
2024-07-10 15:34:31 +0800

    fix:

--- script/sensing.sh
+++ script/sensing.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 source $SCRIPT_DIR/../install/setup.bash 
-source $HOME/pix/robobus/autoware-robobus/install/setup.bash 
 
 function print_help() {
   echo "启动传感器"
@@ -9,13 +8,12 @@ function print_help() {
   echo "Usage:"
   echo "  ./sensing.sh [-robot_state] [-camera_index] [-verify]"
   echo ""
-  echo "    - camera_index : /dev/video0"
-  echo "    - robot_state : 启动传感器是否发布tf树"
+  echo "    - camera_index : /dev/video0 = camera0"
+  echo "    - robot_state : 启动传感器是否发布tf树 true | false"
   echo "    - verify : 标定完成后验证tf"
   echo ""
 }
 
-camera_index="${2:-/dev/video0}"
 robot_state="${1:-true}"
 
-ros2 launch robobus_sensor_kit_calibration sensing_all.launch.xml camera_index:=$camera_index robot_state:=$robot_state
+ros2 launch robobus_sensor_kit_calibration sensing_all.launch.xml robot_state:=$robot_state
---
+++ script/tag_based.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+source $SCRIPT_DIR/../install/setup.bash 
+source $HOME/pix/robobus/autoware-robobus/install/setup.bash 
+
+ros2 launch robobus_sensor_kit_calibration tag_based_pnp_calibrator.launch.xml  camera_name:=camera6
